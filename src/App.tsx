3cd65e43e54baa4abb817a11d2c08d6ff7d15e94
cedric karungu <ckarungu921@gmail.com>
2022-08-27 22:47:08 +0200

    feat: add components pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import './App.css';
 import React from 'react';
 import Home from './pages/Home';
 
+//  a add routing to app, with --home --login --song
+
+// create  context for user auth
+
+
 const App: React.FC = () => {
 	return (
 		<>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+
+// display songs from context
+
+// display adbum from context
 const HomePage: React.FC = () => {
 	return <div>Home</div>;
 };
