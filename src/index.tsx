f29d0fef9ce29ade9114dcc64cf19b04ceb1e4b5
cedric karungu <ckarungu921@gmail.com>
2022-08-30 21:27:11 +0200

    feat: downgrade dep

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import GoogleContextProvider from './context/GoogleContext';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
 	<React.StrictMode>
 		<GoogleContextProvider>
 			<App />
 		</GoogleContextProvider>
-	</React.StrictMode>
+	</React.StrictMode>,
+	document.getElementById('root')!
 );
